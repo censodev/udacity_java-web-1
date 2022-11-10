2d1e3a1159e8480ee77d3646dda5028f86359028
Phuong Bui <phuong.bui.viet@ncs.com.sg>
2022-11-10 17:07:22 +0700

    encryptor update

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/EncryptionService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/EncryptionService.java
@@ -2,10 +2,13 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.*;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
@@ -57,5 +60,14 @@ public class EncryptionService {
             throw new RuntimeException(e);
         }
         return Base64.getEncoder().encodeToString(key);
+    }
+
+    public String encrypt(String data) {
+        var key = genKey();
+        return key + encryptValue(data, key);
+    }
+
+    public String decrypt(String data) {
+        return decryptValue(data.substring(24), data.substring(0, 24));
     }
 }
